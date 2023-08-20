376b7dcd899fa356700c745ba74efc842f0ba206
AbdulRahman Sharief <111624962+AbdulRahman-Sharief@users.noreply.github.com>
2023-08-20 17:19:28 +0300

    initial commit

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,24 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/Billboard";
+import ProductList from "@/components/ProductList";
+import Container from "@/components/ui/Container";
+import React from "react";
+
+export const revalidate = 0;
+const HomePage = async () => {
+  const billboard = await getBillboard("e3826e3c-9f7c-4b0e-9076-0a4b91ec8b0b");
+  const products = await getProducts({ isFeatured: true });
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;

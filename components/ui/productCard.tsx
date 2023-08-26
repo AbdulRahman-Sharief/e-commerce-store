ba868aee01e0618a2b61f73c0d98ff73ab36ff3d
AbdulRahman Sharief <111624962+AbdulRahman-Sharief@users.noreply.github.com>
2023-08-27 02:42:43 +0300

    add to cart functionality

--- components/ui/productCard.tsx
+++ components/ui/productCard.tsx
@@ -7,11 +7,13 @@ import IconButton from "./IconButton";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./Currency";
 import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 interface ProductCardProps {
   data: Product;
 }
 const productCard: React.FC<ProductCardProps> = ({ data }) => {
   //   console.log(data);
+  const cart = useCart();
   const PreviewModal = usePreviewModal();
   const router = useRouter();
   const handleClick = () => {
@@ -20,6 +22,10 @@ const productCard: React.FC<ProductCardProps> = ({ data }) => {
   const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
     event.stopPropagation();
     PreviewModal.onOpen(data);
+  };
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+    cart.addItem(data);
   };
   return (
     <div
@@ -41,7 +47,7 @@ const productCard: React.FC<ProductCardProps> = ({ data }) => {
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
-              onClick={() => {}}
+              onClick={onAddToCart}
               icon={<ShoppingCart size={20} className="text-gray-600" />}
             />
           </div>
